5235ad70ca793bfa99a1e95a50a5f7820142a7a3
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-10-12 20:13:53 +0600

    SEND JWT and VERFY

--- index.js
+++ index.js
@@ -32,65 +32,88 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-     
+
     const usersCollection = client.db("EcommerceDb").collection("users");
     const menuCollection = client.db("EcommerceDb").collection("menu");
     const reviewCollection = client.db("EcommerceDb").collection("review");
     const cartCollection = client.db("EcommerceDb").collection("carts");
 
-   // trying to find users related apis 
+    // verify jwt 
 
-
-   app.get('/users', async(req, res) =>{
-     const result = await usersCollection.find().toArray();
-     res.send(result);
-   })
-
-   app.post('/users', async(req, res)=>{
-    const user = req.body;
- 
-    const query ={email:user.email}
-    const existingUser = await usersCollection.findOne(query);
-
-    if(existingUser){
-      return ({message: " User Already Exists "})
+    const verifyJwt =(req, res, next) =>{
+      const authorization = req.headers.authorization;
+      if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+      }
+      // bearer token
+      const token = authorization.split(' ')[1];
+    
+      jwt.verify(token, process.env.ACCESS_TOKEN_JWT, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
     }
-    const result = await usersCollection.insertOne(user);
-    res.send(result);
 
 
-   }) 
-
-   // access token 
-   app.post('/jwt', (req, res) => {
-    const user = req.body;
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_JWT, { expiresIn: '1h' });
-
-    res.send({token});
 
 
-   })
+
+    // trying to find users related apis 
 
 
-    // trying to get menu collection 
-    app.get('/menu', async(req, res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result);
+    app.get('/users',  async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return ({ message: " User Already Exists " })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
 
 
     })
-    
-     // trying to get reviews collection 
-    app.get('/review', async(req, res)=>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+
+    // access token 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_JWT, { expiresIn: '3h' });
+
+      res.send({ token });
+
+
+    })
+
+
+    // trying to get menu collection 
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+
+
+    })
+
+    // trying to get reviews collection 
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
 
 
     })
 
     // cart collection related kaj
 
-    app.post('/carts', async(req, res) =>{
+    app.post('/carts', async (req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
@@ -98,36 +121,45 @@ async function run() {
     })
 
     // get Cart api data 
-    app.get('/carts', async(req, res)=>{
+    app.get('/carts', verifyJwt,  async (req, res) => {
       const email = req.query.email;
-      if(!email){
+
+      
+
+      if (!email) {
         return ([])
       }
-      const query =  {email:email};
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        res.status(403).send({ error: true, message: 'forbidden Access'});
+      }
+
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
 
     })
 
     // admin 
-    app.patch('/users/admin/:id', async(req, res) =>{
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updateDoc = {
         $set: {
           role: `admin`
         },
       };
 
-      const result = await usersCollection.updateOne(filter,updateDoc)
+      const result = await usersCollection.updateOne(filter, updateDoc)
       res.send(result);
     })
 
-  
+
 
     // delete an item from cart 
 
-    app.delete('/carts:id', async(req, res) =>{
+    app.delete('/carts:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
