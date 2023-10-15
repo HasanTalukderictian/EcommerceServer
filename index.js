9cd6925366cfd3f95e2e67b6155ace325278ccf1
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-10-15 10:30:21 +0600

    install stripe and create stripe payment stripe

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+
 const port = process.env.PORT || 4000;
+
 
 
 // middleware 
@@ -214,7 +217,19 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+    
 
+    // create payment intent 
+    app.post('/create-payment-intent', async(req, res) =>{
+      const {price} = req.body;
+      const amount = price*100;
+      const paymentIntent =await stripe.paymentIntents.create({
+        amount: amount,
+        currency:'usd',
+        payment_method_types: ['card']
+      })
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
